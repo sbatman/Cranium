d6462a1390921b5c08a110f78f59a968c20a7f0e
S_Batman <steven@nerfgames.co.uk>
2013-04-07 22:56:33 +0100

    Basic logic for the layer base class

--- Cranium/Structure/Layer/Base.cs
+++ Cranium/Structure/Layer/Base.cs
@@ -5,13 +5,44 @@
 // 
 // //////////////////////
 using System;
+using System.Collections.Generic;
 
 namespace Structure.Layer
 {
 	public class Base
 	{
-		public Base ()
+		protected List<Node.Base> _Nodes;
+		protected List<Layer.Base> _ForwardConnectedLayers;
+		protected List<Layer.Base> _ReverseConnectedLayers;
+		
+		public virtual void SetNodes (List<Node.Base> nodes)
 		{
+			_Nodes = nodes;	
+		}
+		
+		public virtual List<Node.Base> GetNodes()
+		{
+			return _Nodes;	
+		}
+
+		public virtual void ConnectFowardLayer (Layer.Base layer)
+		{
+			_ForwardConnectedLayers.Add (layer);
+		}
+		
+		public virtual List<Layer.Base> GetForwardConnectedLayers ()
+		{
+			return _ForwardConnectedLayers;	
+		}
+
+		public virtual void ConnectReverseLayer (Layer.Base layer)
+		{
+			_ReverseConnectedLayers.Add (layer);
+		}
+		
+		public virtual List<Layer.Base> GetReverseConnectedLayers ()
+		{
+			return _ReverseConnectedLayers;
 		}
 	}
 }
