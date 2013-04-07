1430fb9110ee772922b45552af54057fefa3f306
S_Batman <steven@nerfgames.co.uk>
2013-04-07 15:25:34 +0100

    Minor change to change log support and commenting of the Activation Base Class

--- Cranium/Structure/ActivationFunction/Base.cs
+++ Cranium/Structure/ActivationFunction/Base.cs
@@ -10,7 +10,22 @@ namespace Structure.ActivationFunction
 {
 	public class Base
 	{
+		/// <summary>
+		/// Returns the input after running through the activation function.
+		/// </summary>
+		/// <param name='input'>
+		/// The value to pass to the activation function
+		/// </param>
 		public abstract Double Compute(Double input);
+		/// <summary>
+		/// Computes the derivative using the activation function.
+		/// </summary>
+		/// <returns>
+		/// The derivative.
+		/// </returns>
+		/// <param name='input'>
+		/// Input.
+		/// </param>
 		public abstract Double ComputeDerivative(Double input);
 	}
 }
