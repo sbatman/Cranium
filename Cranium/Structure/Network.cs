82a8da86b058425201f20288e4e04541b819a4b9
sbatman <steven@nerfgames.co.uk>
2013-04-11 19:52:15 +0100

    Added further commenting and the base function of the structure update in the network structure class

--- Cranium/Structure/Network.cs
+++ Cranium/Structure/Network.cs
@@ -17,18 +17,40 @@ namespace Structure
 			_CurrentLayers = new List<Layer.Base> ();
 		}
 		
+		/// <summary>
+		/// Adds a layer to the NeuralNetwork Structure causing a structure update 
+		/// </summary>
+		/// <param name='newLayer'>
+		/// New layer.
+		/// </param>
 		public void AddLayer (Layer.Base newLayer)
 		{
 			if (_CurrentLayers.Contains (newLayer))
 				return;
 			_CurrentLayers.add (newLayer);	
+			StructureUpdate();
 		}
 		
+		/// <summary>
+		/// Removes a layer from the neuralNetwork Structure causing a structure update 
+		/// </summary>
+		/// <param name='layer'>
+		/// Layer.
+		/// </param>
 		public void RemoveLayer (Layer.Base layer)
 		{
 			if (!_CurrentLayers.Contains (layer))
 				return;
 			_CurrentLayers.Remove (layer);
+			StructureUpdate();
+		}
+		
+		/// <summary>
+		/// Called when a change is detected to this level of the nerual netowrks structure
+		/// </summary>
+		protected virtual void StructureUpdate()
+		{
+			
 		}
 	}
 }
