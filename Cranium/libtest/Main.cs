db545e1dbc62769da64742f49dfbd18001ad75e8
sbatman <steven@nerfgames.co.uk>
2013-04-14 02:20:32 +0100

    Minor code changes

--- Cranium/libtest/Main.cs
+++ Cranium/libtest/Main.cs
@@ -6,32 +6,44 @@ namespace Cranium.libtest
 	class MainClass
 	{
 		Network TestNetworkStructure;
+
 		public static void Main (string[] args)
 		{
-			TestNetworkStructure = new Network();
+			TestNetworkStructure = new Network ();
+			BuildStructure();
 		}
-		public static void BuildStructure()
+
+		public static void BuildStructure ()
 		{
-			Cranium.Structure.Layer.Base InputLayer = new Cranium.Structure.Layer.Base();
-			List<Cranium.Structure.Node.Base> InputLayerNodes = new List<Cranium.Structure.Node.Base>();
-			for(int i=0; i<2; i++)InputLayerNodes.Add(new Cranium.Structure.Node.Base(InputLayer,new Cranium.Structure.ActivationFunction.Linear()));
-			InputLayer.SetNodes(InputLayerNodes);
+			Cranium.Structure.Layer.Base InputLayer = new Cranium.Structure.Layer.Base ();
+			List<Cranium.Structure.Node.Base> InputLayerNodes = new List<Cranium.Structure.Node.Base> ();
+			for (int i=0; i<2; i++)
+				InputLayerNodes.Add (new Cranium.Structure.Node.Base (InputLayer, new Cranium.Structure.ActivationFunction.Linear ()));
+			InputLayer.SetNodes (InputLayerNodes);
 			
-			Cranium.Structure.Layer.Base HiddenLayer = new Cranium.Structure.Layer.Base();
-			List<Cranium.Structure.Node.Base> HiddenLayerNodes = new List<Cranium.Structure.Node.Base>();
-			for(int i=0; i<10; i++)HiddenLayerNodes.Add(new Cranium.Structure.Node.Base(HiddenLayer,new Cranium.Structure.ActivationFunction.BinarySigmoid()));
-			HiddenLayer.SetNodes(HiddenLayerNodes);
+			Cranium.Structure.Layer.Base HiddenLayer = new Cranium.Structure.Layer.Base ();
+			List<Cranium.Structure.Node.Base> HiddenLayerNodes = new List<Cranium.Structure.Node.Base> ();
+			for (int i=0; i<10; i++)
+				HiddenLayerNodes.Add (new Cranium.Structure.Node.Base (HiddenLayer, new Cranium.Structure.ActivationFunction.BinarySigmoid ()));
+			HiddenLayer.SetNodes (HiddenLayerNodes);
 			
-			Cranium.Structure.Layer.Base OutputLayer = new Cranium.Structure.Layer.Base();
-			List<Cranium.Structure.Node.Base> OuputLayerNodes = new List<Cranium.Structure.Node.Base>();
-			for(int i=0; i<1; i++)OuputLayerNodes.Add(new Cranium.Structure.Node.Base(OutputLayer,new Cranium.Structure.ActivationFunction.Linear()));
-			HiddenLayer.SetNodes(OuputLayerNodes);
+			Cranium.Structure.Layer.Base OutputLayer = new Cranium.Structure.Layer.Base ();
+			List<Cranium.Structure.Node.Base> OuputLayerNodes = new List<Cranium.Structure.Node.Base> ();
+			for (int i=0; i<1; i++)
+				OuputLayerNodes.Add (new Cranium.Structure.Node.Base (OutputLayer, new Cranium.Structure.ActivationFunction.Linear ()));
+			HiddenLayer.SetNodes (OuputLayerNodes);
 			
-			TestNetworkStructure.AddLayer(InputLayer);
-			InputLayer.ConnectFowardLayer(HiddenLayer);
-			TestNetworkStructure.AddLayer(HiddenLayer);
-			HiddenLayer.ConnectFowardLayer(OutputLayer);
-			TestNetworkStructure.AddLayer(OutputLayer);
+			InputLayer.ConnectFowardLayer (HiddenLayer);
+			HiddenLayer.ConnectFowardLayer (OutputLayer);
+			
+			TestNetworkStructure.AddLayer (InputLayer);			
+			TestNetworkStructure.AddLayer (HiddenLayer);			
+			TestNetworkStructure.AddLayer (OutputLayer);
+			
+			foreach (Cranium.Structure.Layer.Base layer in TestNetworkStructure.GetCurrentLayers())
+				layer.PopulateNodeConnections ();
+			
+						
 		}
 	}
 }
