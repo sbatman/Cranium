fa81854f93d61b766042fe6aea5829c8e682a32b
sbatman <steven@nerfgames.co.uk>
2013-04-18 00:33:58 +0100

    Updated the file headers to reflect the nature of the licence and configured it as default on the SLN

--- Cranium/Structure/Node/Bias.cs
+++ Cranium/Structure/Node/Bias.cs
@@ -1,9 +1,14 @@
-// ///////////////////////
-// 
-// Cranium - A neural network framework for C#
-// https://github.com/sbatman/Cranium.git
-// 
-// //////////////////////
+// // ///////////////////////
+// // 
+// // Cranium - A neural network framework for C#
+// // https://github.com/sbatman/Cranium.git
+// // 
+// // This work is covered under the Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) licence.
+// // More information can be found about the liecence here http://creativecommons.org/licenses/by-sa/3.0/
+// // If you wish to discuss the licencing terms please contact Steven Batchelor-Manning
+// //
+// // //////////////////////
+
 using System;
 using Cranium.Structure.Node;
 
@@ -14,7 +19,7 @@ namespace Cranium.Structure.Node
 		public Bias (Layer.Base parentLayer, ActivationFunction.Base activationFunction) : base(parentLayer,activationFunction)
 		{
 		}
-
+		
 		public override Double GetValue ()
 		{
 			return 1;
