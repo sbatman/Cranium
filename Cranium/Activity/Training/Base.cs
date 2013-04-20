c4a7daadf6d0e22cdbd5d187779b94db75212d0b
sbatman <steven@nerfgames.co.uk>
2013-04-20 18:24:01 +0100

    Added the base class for the training activity

---
+++ Cranium/Activity/Training/Base.cs
@@ -0,0 +1,56 @@
+// // ///////////////////////
+// // 
+// // Cranium - A neural network framework for C#
+// // https://github.com/sbatman/Cranium.git
+// // 
+// // This work is covered under the Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) licence.
+// // More information can be found about the liecence here http://creativecommons.org/licenses/by-sa/3.0/
+// // If you wish to discuss the licencing terms please contact Steven Batchelor-Manning
+// //
+// // //////////////////////
+using System.Threading;
+
+using System;
+
+namespace Cranium.Activity.Training
+{
+	public abstract class Base
+	{
+		private bool _Running = false;
+		private bool _Stopping = false;
+		private Thread _LoopThread;
+		protected Structure.Network _TargetNetwork;
+
+		public void Start ()
+		{
+			_LoopThread = new Thread (_Tick);
+			_LoopThread.Start ();
+		}
+		
+		public virtual void SetTargetNeteork (Structure.Network targetNetwork)
+		{
+			_TargetNetwork = targetNetwork;	
+		}		
+
+		public bool IsRunning ()
+		{
+			return _Running;
+		}
+
+		public void Stop ()
+		{
+			_Stopping = true;
+		}
+
+		protected abstract bool _Tick ();
+
+		private void UpdateLoop ()
+		{
+			_Running = true;
+			while (_Tick()&&!_Stopping)
+				;
+			_Running = false;
+		}
+	}
+}
+
