66287fa5dbdf80aa28676a5fea79d0179099e64c
sbatman <steven@nerfgames.co.uk>
2013-05-04 15:44:12 +0100

    commenting of the Recurrent-context layer

--- Cranium/Structure/Layer/Recurrent-Context.cs
+++ Cranium/Structure/Layer/Recurrent-Context.cs
@@ -13,6 +13,11 @@ using System.Collections.Generic;
 
 namespace Cranium.Structure.Layer
 {
+	/// <summary>
+	/// This type of layer offers a form of recursive memory in the form of gradually lower momentum recursive stores.
+	/// This is described in detail in Ulbrich, C., 1994. Multi-Recurrent Netowrks for Traffic Forecasting. Vienna: Austrian Research Institute for Artificial Intelligence.
+	/// The white paper can be found here http://www.aaai.org/Papers/AAAI/1994/AAAI94-135.pdf
+	/// </summary>
 	public class Recurrent_Context : Base
 	{
 		protected List<Node.Base> _SourceNodes;
@@ -23,13 +28,19 @@ namespace Cranium.Structure.Layer
 			_SourceNodes = new List<Node.Base> ();
 			_LevelOfContext = levelOfContext;
 		}
-
+		
+		/// <summary>
+		/// Populates the node connections aka the weights between nodes.
+		/// </summary>
 		public override void PopulateNodeConnections ( )
 		{
 			BuildNodeBank ();
 			base.PopulateNodeConnections ();
 		}
-
+		
+		/// <summary>
+		/// Builds the node bank that creates recursion, The number of banks (and thus recursive steps) is set during the contructor of the layer.
+		/// </summary>
 		public virtual void BuildNodeBank ( )
 		{
 			double step = 1d / _LevelOfContext;
@@ -42,11 +53,20 @@ namespace Cranium.Structure.Layer
 			}
 		}
 
+		/// <summary>
+		/// Adds the source nodes from which the node banks will be built and connected.
+		/// </summary>
+		/// <param name='nodes'>
+		/// Nodes.
+		/// </param>
 		public virtual void AddSourceNodes ( List<Node.Base> nodes )
 		{
 			_SourceNodes.AddRange ( nodes );
 		}
 		
+		/// <summary>
+		/// Performs any extra update required on child nodes
+		/// </summary>
 		public override void UpdateExtra ( )
 		{
 			foreach ( Node.Recurrent_Context n in _Nodes )
