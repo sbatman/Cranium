ab7f3f528267afe583d2379a24f1ab4e5be1ec24
sbatman <steven@nerfgames.co.uk>
2013-05-29 09:32:10 +0100

    Added further settings for network logic

--- Cranium/Lobe.Worker/SettingsLoader.cs
+++ Cranium/Lobe.Worker/SettingsLoader.cs
@@ -8,7 +8,9 @@ namespace Cranium.Lobe.Worker
 {
     static class SettingsLoader
     {
-        public static int WorkerCount = -1;
+        public static int WorkerThreadCount = -1;
+        public static string CommsLocalIP = "";
+        public static int CommsLocalPort = 0;
         /// <summary>
         /// Loads in the settings file for the worker agent, this will build a dictionary of variables to be used.
         /// </summary>
@@ -29,12 +31,26 @@ namespace Cranium.Lobe.Worker
 
             if (dictionaryOfSettings.Count == 0) throw (new Exception("No settings present in file"));
 
-            if (dictionaryOfSettings.ContainsKey("WorkerCount"))
+            if (dictionaryOfSettings.ContainsKey("WorkerThreadCount"))
             {
                 int count = 0;
-                if (!int.TryParse(dictionaryOfSettings["WorkerCount"], out count)) throw (new Exception("Error parsing WorkerCount"));
-                if (count < 1 || count > 255) throw (new Exception("Invalid WorkerCount specified"));
-                WorkerCount = count;
+                if (!int.TryParse(dictionaryOfSettings["WorkerThreadCount"], out count)) throw (new Exception("Error parsing WorkerThreadCount"));
+                if (count < 1 || count > 255) throw (new Exception("Invalid WorkerThreadCount specified"));
+                WorkerThreadCount = count;
+            }
+
+            if (dictionaryOfSettings.ContainsKey("LocalIP"))
+            {
+                if (dictionaryOfSettings["LocalIP"].Length ==0 ) throw (new Exception("LocalIP not correctly specified"));
+                CommsLocalIP = dictionaryOfSettings["LocalIP"];
+            }
+
+            if (dictionaryOfSettings.ContainsKey("Port"))
+            {
+                int port = 0;
+                if (!int.TryParse(dictionaryOfSettings["Port"], out port)) throw (new Exception("Error parsing Port"));
+                if (port < 1000 || port > 36000) throw (new Exception("Invalid Port specified, must be within 1000-36000"));
+                CommsLocalPort = port;
             }
 
             return true;
