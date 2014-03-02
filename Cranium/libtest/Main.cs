7bb608b38210564fa32d29cf9849bf5765dae184
sbatman <steven@nerfgames.co.uk>
2014-03-02 22:32:14 +0000

    Cleaned up the tests a little

--- Cranium/libtest/Main.cs
+++ Cranium/libtest/Main.cs
@@ -1,14 +1,14 @@
 #region info
 
 // //////////////////////
-//  
+//
 // Cranium - A neural network framework for C#
 // https://github.com/sbatman/Cranium.git
-// 
+//
 // This work is covered under the Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) licence.
 // More information can be found about the liecence here http://creativecommons.org/licenses/by-sa/3.0/
 // If you wish to discuss the licencing terms please contact Steven Batchelor-Manning
-// 
+//
 // //////////////////////
 
 #endregion
@@ -43,29 +43,33 @@ namespace Cranium.Lib.Test
             Console.WriteLine("4 -  RNNTest2");
             Console.WriteLine("5 -  MG Recurrent Test");
             Console.WriteLine("6 -  MG Echo State Test");
+            Console.WriteLine("7 -  MG Echo State Test on Lobe");
 
             ConsoleKey pressedKey = Console.ReadKey().Key;
             Console.Clear();
             switch (pressedKey)
             {
-                case ConsoleKey.D1:
-                    XOR2Test.Run();
-                    break;
-                case ConsoleKey.D2:
-                    XOR3Test.Run();
-                    break;
-                case ConsoleKey.D3:
-                    RNNTest.Run();
-                    break;
-                case ConsoleKey.D4:
-                    RNNTest2.Run();
-                    break;
-                case ConsoleKey.D5:
-                    MG_Recurrent_Test.Run();
-                    break;
-                case ConsoleKey.D6:
-                    MG_EchoState_Test.Run();
-                    break;
+            case ConsoleKey.D1:
+                XOR2Test.Run();
+                break;
+            case ConsoleKey.D2:
+                XOR3Test.Run();
+                break;
+            case ConsoleKey.D3:
+                RNNTest.Run();
+                break;
+            case ConsoleKey.D4:
+                RNNTest2.Run();
+                break;
+            case ConsoleKey.D5:
+                MG_Recurrent_Test.Run();
+                break;
+            case ConsoleKey.D6:
+                MG_EchoState_Test.Run();
+                break;
+            case ConsoleKey.D7:
+                MG_EchoState_Test_Lobe.Run();
+                break;
             }
         }
     }
